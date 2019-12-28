a74c131960fe5feb44cf92ab4fc1305b1c735c01
Toumani <toumani49@gmail.com>
2019-12-28 15:07:47 +0100

    Initial commit

---
+++ src/main/kotlin/com/tripplanner/api/VehicleSpecsServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.tripplanner.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VehicleSpecsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<VehicleSpecsServiceApplication>(*args)
+}
